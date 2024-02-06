06e380774a4403e971ff05a14c9978ac2a25ee03
HarshKunte <harshkunte99@gmail.com>
2024-02-06 13:01:51 +0530

    added routes to title of each category

--- src/components/menu-category-item/menu-category-item.component.js
+++ src/components/menu-category-item/menu-category-item.component.js
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import './menu-category-item.styles.scss'
 
 const MenuCategoryItem = ({category}) => {
-    const {title, imageUrl} = category
+    const {title, imageUrl, route} = category
+
+    const navigate  = useNavigate()
+    const navigateHandler = () => navigate(route)
     return (
-        <div  className="menu-category-container">
+        <div  className="menu-category-container" onClick={navigateHandler}>
           <div className="background-image" style={{
             backgroundImage : `url(${imageUrl})`
           }} />
